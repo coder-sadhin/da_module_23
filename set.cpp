036b1304db92c7e6009da6a7e90f4173b49f0024
coder-sadhin <codersadhin@gmail.com>
2024-08-29 20:28:31 +0600

    set complete

--- set.cpp
+++ set.cpp
@@ -5,6 +5,20 @@ int main()
     set<int> s;
     int n;
     cin >> n;
-    
+    while (n--)
+    {
+        int x;
+        cin >> x;
+        s.insert(x); // O(logN)
+    }
+    cout << s.count(100) << endl; // O(logN)
+    if (s.count(1000))
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+    // for (auto it = s.begin(); it != s.end(); it++)
+    // {
+    //     cout << *it << endl;
+    // }
     return 0;
 }
